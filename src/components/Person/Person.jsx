d50fe4c1ee923c9ec1ec8fd8afc420fc65e4d588
Andrew Pavlenko <andrew.pawllenko@gmail.com>
2025-05-13 20:33:27 +0300

    fix the problem

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
@@ -11,7 +9,7 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
 
-      {age && <p className="Person__age">I am {age}</p>}
+      {age != null && <p className="Person__age">I am {age}</p>}
 
       <p className="Person__partner">{partner}</p>
     </section>
